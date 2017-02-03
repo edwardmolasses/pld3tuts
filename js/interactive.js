f8cf3dbab449a87ba2d324f24ac6a7cbff395334
Andrew Lachkovics <andrew.lachkovics@azubu.com>
2017-02-02 22:37:54 -0800

    refactor

--- js/interactive.js
+++ js/interactive.js
@@ -11,38 +11,50 @@ function getDate(d) {
 	return new Date(year, month, day);
 }
 
-function buildLine(ds) {
-	var minDate = getDate(ds.monthlySales[0]['month']);
-	var maxDate = getDate(ds.monthlySales[ds.monthlySales.length-1]['month']);
-	var xScale = d3.time.scale()
-		.domain([minDate, maxDate ])
-		.range([padding+5, w-padding])
-		.nice();
-	var yScale = d3.scale.linear()
-		.domain([0, d3.max(ds.monthlySales, function(d) { return d.sales; }) ])
-		.range([h-padding, 10])
-		.nice();
-	var xAxisGen = d3.svg.axis().scale(xScale).orient("bottom").tickFormat(d3.time.format("%b"));
-	var yAxisGen = d3.svg.axis().scale(yScale).orient("left").ticks(4);
-
-	var lineFun = d3.svg.line()
+var getLineFun = function(xScale, yScale) {
+	return d3.svg.line()
 		.x(function(d) { return xScale(getDate(d.month)); })
 		.y(function(d) { return yScale(d.sales); })
 		.interpolate("linear");
+}
 
-	var svg = d3.select("body").append("svg").attr({width: w, height: h, "id": "svg-"+ds.category}); 
+function getScale(min, max, aRange, bRange) {
+	return d3.time.scale()
+		.domain([min, max ])
+		.range([aRange, bRange])
+		.nice();
+}
+
+function getAxisGen(scale, orient, tickFormat, ticks) {
+	return d3.svg.axis().scale(scale).orient(orient).tickFormat(tickFormat).ticks(ticks);
+}
+
+function getSvg(attr) {
+	return d3.select("body").append("svg").attr(attr);
+}
+
+function buildLine(ds) {
+	var minDate = getDate(ds.monthlySales[0]['month']);
+	var maxDate = getDate(ds.monthlySales[ds.monthlySales.length-1]['month']);
+	var xScale = getScale(minDate, maxDate, padding+5, w-padding);
+	var yScale = getScale(0, d3.max(ds.monthlySales, function(d) { return d.sales; }), h-padding, 10);
+	var xAxisGen = getAxisGen(xScale, "bottom", d3.time.format("%b"), null);
+	var yAxisGen = getAxisGen(yScale, "left", null, 4);
+	var lineFun = getLineFun(xScale, yScale);
+
+	var svg = d3.select("body").append("svg").attr({width: w, height: h, "id": "svg-"+ds.category});
 
 	var yAxis = svg.append("g").call(yAxisGen)
 				.attr("class", "y-axis")
-				.attr("transform", "translate(" + padding + ",0)"); 
+				.attr("transform", "translate(" + padding + ",0)");
 
 	var xAxis = svg.append("g").call(xAxisGen)
 				.attr("class", "x-axis")
-				.attr("transform", "translate(0," + (h - padding) + ")"); 
+				.attr("transform", "translate(0," + (h - padding) + ")");
 
 	var viz = svg.append("path")
 				.attr({
-					d: lineFun(ds.monthlySales), 
+					d: lineFun(ds.monthlySales),
 					"class": "path-" + ds.category
 				});
 }
@@ -50,23 +62,13 @@ function buildLine(ds) {
 function updateLine(ds) {
 	var minDate = getDate(ds.monthlySales[0]['month']);
 	var maxDate = getDate(ds.monthlySales[ds.monthlySales.length-1]['month']);
-	var xScale = d3.time.scale()
-		.domain([minDate, maxDate ])
-		.range([padding+5, w-padding])
-		.nice();
-	var yScale = d3.scale.linear()
-		.domain([0, d3.max(ds.monthlySales, function(d) { return d.sales; }) ])
-		.range([h-padding, 10])
-		.nice();
-	var xAxisGen = d3.svg.axis().scale(xScale).orient("bottom").tickFormat(d3.time.format("%b")).ticks(ds.monthlySales.length-1);
-	var yAxisGen = d3.svg.axis().scale(yScale).orient("left").ticks(4);
+	var xScale = getScale(minDate, maxDate, padding+5, w-padding);
+	var yScale = getScale(0, d3.max(ds.monthlySales, function(d) { return d.sales; }), h-padding, 10);
+	var xAxisGen = getAxisGen(xScale, "bottom", d3.time.format("%b"), ds.monthlySales.length - 1);
+	var yAxisGen = getAxisGen(xScale, "left", null, 4);
+	var lineFun  = getLineFun(xScale, yScale);
 
-	var lineFun  = d3.svg.line()
-		.x(function(d) { return xScale(getDate(d.month)); })
-		.y(function(d) { return yScale(d.sales); })
-		.interpolate("linear");
-
-	var svg = d3.select("body").select("#svg-" + ds.category); 
+	var svg = d3.select("body").select("#svg-" + ds.category);
 
 	var yAxis = svg.selectAll("g.y-axis").call(yAxisGen);
 
@@ -99,7 +101,7 @@ d3.json("https://api.github.com/repos/bsullins/d3js-resources/contents/monthlySa
 
 			decodedData.contents.forEach(function(ds){
 				ds.monthlySales.splice(0,ds.monthlySales.length-sel);
-				updateLine(ds); 
+				updateLine(ds);
 			});
 		});
 });
